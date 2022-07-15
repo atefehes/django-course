ddffaac56c5b63cd302bfcb514889e7284fec86a
Atefeh  Atefeh <atyroy01@gmail.com>
2022-07-15 14:42:50 +0000

    Finish django registration

--- blango/urls.py
+++ blango/urls.py
@@ -19,6 +19,9 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 import blog.views
+from django_registration.backends.activation.views import RegistrationView
+from blango_auth.forms import BlangoRegistrationForm
+import blango_auth.views
 
 # from django.conf import settings
 # print(f"Time zone: {settings.TIME_ZONE}")
@@ -30,7 +33,18 @@ urlpatterns = [
     path("api/v1/", include("blog.api_urls")),
 
     path("", blog.views.index),
-    path("ip/", blog.views.get_ip)
+    path("ip/", blog.views.get_ip),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/profile/", blango_auth.views.profile, name="profile"),
+    path("accounts/", include("django_registration.backends.activation.urls")),
+    path(
+    "accounts/register/",
+    RegistrationView.as_view(form_class=BlangoRegistrationForm),
+    name="django_registration_register",
+    ),
+
+
+
 ]
 
 
