f82286347d0bf2badad712bbe27661be3cb058ad
Atefeh  Atefeh <atyroy01@gmail.com>
2022-07-20 16:01:08 +0000

    Finish DRF views

--- blango/urls.py
+++ blango/urls.py
@@ -30,7 +30,8 @@ import blango_auth.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
-    path("api/v1/", include("blog.api_urls")),
+    path("api/v1/", include("blog.api.urls")),
+
 
     path("", blog.views.index),
     path("ip/", blog.views.get_ip),
